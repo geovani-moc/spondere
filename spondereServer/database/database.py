87af5b0608628074ef13083d530d3321a24bfb81
geovani-moc <geovani.moc@outlook.com>
2021-10-20 16:15:59 -0300

    pequenas correções

--- spondereServer/database/database.py
+++ spondereServer/database/database.py
@@ -1,6 +1,6 @@
 from typing import ContextManager
 import psycopg2
-from psycopg2 import pool
+from psycopg2 import connect, pool
 from config import(
     MIN_CONNECTIONS,
     MAX_CONNECTIONS,
@@ -20,14 +20,32 @@ postgresSQL = psycopg2.pool.ThreadedConnectionPool(
         port = PORT,
         database=DB_NAME
     )
-if not postgresSQL:
-    print("Erro ao tentar se conectar ao BD postgresSQL.")
 
+# @ContextManager
+# def getConnection():
+#     connection = postgresSQL.getconn()
+#     try:
+#         yield connection
+#     finally:
+#         postgresSQL.putconn(connection)
 
-@ContextManager
-def getConnection():
-    connection = postgresSQL.getconn()
-    try:
-        yield connection
-    finally:
-        postgresSQL.putconn(connection)
+# def getConnectio():
+#     if postgresSQL:
+#         return postgresSQL.getconn(), None
+#     else:
+#         return None, "Erro ao estabelece conexão pool."
+
+def newConnection():
+    postgresSQL = psycopg2.pool.ThreadedConnectionPool(
+        MIN_CONNECTIONS,
+        MAX_CONNECTIONS,
+        user = DB_USERNAME,
+        password = DB_PASSWORD,
+        host = HOST,
+        port = PORT,
+        database=DB_NAME
+    )
+    if not postgresSQL:
+        return ("Erro ao tentar se conectar ao BD postgresSQL.")
+    
+    return None
--- spondereServer/database/user.py
+++ spondereServer/database/user.py
@@ -1,6 +1,11 @@
+from logging import error
+from time import process_time
+
+from passlib.utils import repeat_string
 from entity.user import User
-from database.database import getConnection
+from database.database import postgresSQL
 from passlib.context import CryptContext
+from psycopg2 import DatabaseError
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -17,29 +22,33 @@ def read(code: str):
 def delete(code: str):
     return None
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
 
 # def getPassword_hash(password):
 #     return pwd_context.hash(password)
 
 def checkUser(data: User):
-    connection = getConnection()
-    cur = connection.cursor()
+    if data.userName is None or data.password is None:
+        return False
+
+    user:User
     sql_query = 'select username, password from users where username = %s'
-    cur.execute(sql_query, (User.userName, ))
+
+    try:
+        connection = postgresSQL.getconn()
+
+        if(connection):
+            cur = connection.cursor()
+            cur.execute(sql_query, (data.userName, ))
+            user.userName, user.password = cur.fetchone()
+
+            postgresSQL.putconn(connection)
+
+    except (Exception, DatabaseError) as error:
+        print("Erro de conexão com o banco de dados: ", error)
     
-    for user in fake_users_db:
-        if user.userName == data.userName and verifyPassword(user.password, data.password):
-            return True
+    if user.userName == data.userName and verifyPassword(user.password, data.password):
+        return True
+
     return False
 
 
